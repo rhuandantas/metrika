8abf2fb0989cc2ba8518e26d346453da82ae90e0
rhuan <rhuannixon@gmail.com>
2025-08-27 19:25:24 -0300

    initial implementation of the smartblox client with basic status and block retrieval

---
+++ go.mod
@@ -0,0 +1,15 @@
+module github.com/rhuandantas/metrika
+
+go 1.24.4
+
+require (
+	github.com/Metrika-Inc/smartblox v0.0.0-20250826172911-dc4a04e5a8da
+	github.com/rs/zerolog v1.34.0
+)
+
+require (
+	github.com/jinzhu/copier v0.4.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
+	golang.org/x/sys v0.12.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/Metrika-Inc/smartblox"
+)
+
+type Status struct {
+	LastRound int64 `json:"last-round"`
+}
+
+type Transaction struct {
+	Amount int64  `json:"amount"`
+	Sender int64  `json:"sender"`
+	Type   string `json:"type"`
+	// Handle both spellings: recipient & receipient.
+	Recipient int64 `json:"recipient"`
+	// Receipient is captured only for JSON decoding compatibility.
+	Receipient int64 `json:"receipient"`
+}
+
+type TransactionEnvelope struct {
+	Sig string      `json:"sig"`
+	Tx  Transaction `json:"tx"`
+}
+
+// Block models /api/blocks/{round}
+type Block struct {
+	Round int64                 `json:"round"`
+	Txs   []TransactionEnvelope `json:"txs"`
+}
+
+func main() {
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
+
+	go func() {
+		if err := run(ctx); err != nil {
+			log.Fatal("Server error: ", err)
+		}
+		stop()
+	}()
+
+	<-ctx.Done()
+	log.Print("Shutting down server gracefully...")
+
+}
+
+func run(ctx context.Context) error {
+	round, err := smartblox.GetStatus()
+	if err != nil {
+		return err
+	}
+	var s Status
+	err = json.Unmarshal(round, &s)
+	if err != nil {
+		return err
+	}
+
+	block, err := smartblox.GetBlock(s.LastRound)
+	if err != nil {
+		return err
+	}
+
+	var b Block
+	err = json.Unmarshal(block, &b)
+	if err != nil {
+		return err
+	}
+
+	log.Printf("Block Round: %d", b.Round)
+	for _, tx := range b.Txs {
+		if tx.Tx.Type == "txfer" {
+			log.Printf("Tx Sig: %s, Sender: %d, Recipient: %d, Amount: %d, Type: %s",
+				tx.Sig, tx.Tx.Sender, tx.Tx.GetRecipient(), tx.Tx.Amount, tx.Tx.Type)
+		}
+	}
+
+	return nil
+}
+
+func (t *Transaction) GetRecipient() int64 {
+	if t.Recipient != 0 {
+		return t.Recipient
+	}
+	return t.Receipient
+}
